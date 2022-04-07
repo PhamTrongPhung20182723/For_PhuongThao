368f8aff43119ab952b66e459d698717de809cbe
PhamTrongPhung20182723 <84733314+PhamTrongPhung20182723@users.noreply.github.com>
2022-04-07 12:07:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chào Yến nhaaa!',
+    introDesc: `Anh có muốn số điều muốn hỏi em, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Vânggg',
+    title: 'Làm người yêu anh nhaaa 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là em đã đồng ý rồi nha :> ',
+    btnYes: 'Hiii, vâng ạ <3 <3 <3',
+    btnNo: 'anh mơ đi nhaaa',
+    question:'Nói cho anh biết tại sao em đồng ý điiii<3',
+    btnReply: 'Gửi anh <3',
+    reply: 'Vì anh tốt bụng, thông minh và luôn quan tâm tới em <3',
+    mess: 'Anh biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'ib với anh luôn đi nàooooo',
+    btnAccept: 'Vângggg <3',
+    messLink: 'https://m.me/phung1609/'
+}
